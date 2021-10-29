9a81a3996425e901976acae787e0244392db4aa8
Lindembergh Madruga <lindemberghferreira@gmail.com>
2021-10-29 09:31:53 -0300

    Criando conexão com o database PostgreSQL

---
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJDBC.java
@@ -0,0 +1,13 @@
+package pos_java_jdbc.pos_java_jdbc;
+
+import org.junit.Test;
+
+import conexao_jdbc.SingleConnection;
+
+public class TesteBancoJDBC {
+
+	@Test
+	public void initBanco() {
+		SingleConnection.getConnection();
+	}
+}
