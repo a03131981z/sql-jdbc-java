9ae054c5b49c476cd99263f82611f7b3628fde5c
Lindembergh Madruga <lindemberghferreira@gmail.com>
2021-11-02 17:11:33 -0300

    Criando nossa estrutura de DAO de acesso ao banco de dados

---
+++ src/main/java/model/UserPosJava.java
@@ -0,0 +1,32 @@
+package model;
+
+public class UserPosJava {
+
+	private Long id;
+	private String nome;
+	private String email;
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getemail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
